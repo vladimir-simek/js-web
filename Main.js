51fa01c9479698ceaa90dfcdbcd0db9067d8d167
vladimir-simek <minervladik@gmail.com>
2021-03-20 19:58:24 +0100

    done

---
+++ Main.js
@@ -0,0 +1,39 @@
+let box = "<div class='box'>"
+let closeDiv = "</div>"
+let sizeBox = 8;
+let box1 = "<div class='box1'>"
+let box2 = "<div class='box2'>"
+
+function writeBox1() {
+    document.write(box1)
+}
+
+function writeBox2() {
+    document.write(box2)
+}
+
+function writeBox() {
+    document.write(box)
+}
+
+function endDiv() {
+    document.write(closeDiv)
+}
+
+function createBox(sizeBox) {
+
+    document.write("<div class='name'><a CLASS='YES'>CHESS TABLE IS OVER</a><div><a class='YES'>HERE IF YOU ARE BLIND:</a></div></div></div>")
+
+    writeBox()
+    for (let i = 0; i < sizeBox; i++) {
+        writeBox1()
+        for (let j = 0; j < sizeBox; j++) {
+            writeBox2()
+            endDiv()
+        }
+        endDiv()
+    }
+    endDiv()
+}
+
+createBox(8)
